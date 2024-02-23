9c4c9cf439a1f8f72bcab09912b94500a51e721e
prathamgyanani <gypratham@gmail.com>
2024-02-23 15:25:42 +0530

    SDK

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros-vm/ROS_Lidar/src
+# Build directory: /home/ros-vm/ROS_Lidar/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ydlidar_ros_driver")
---
+++ build/ydlidar_ros_driver/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros-vm/ROS_Lidar/src/ydlidar_ros_driver
+# Build directory: /home/ros-vm/ROS_Lidar/build/ydlidar_ros_driver
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
